26e77097f898d0e21d46299ccd39f1f0fbe9170e
Beder Bourahmah <bederbourahmah@gmail.com>
2020-11-14 10:36:11 -0800

    Now rendering a basic player shape for side scroller.

--- PA8/SideScroller.cpp
+++ PA8/SideScroller.cpp
@@ -1,7 +1,14 @@
 #include "SideScroller.h"
 
+SideScroller::SideScroller()
+{
+	playerShape = sf::RectangleShape(sf::Vector2f(50, 50));
+	playerShape.setFillColor(sf::Color::Green);
+}
+
 void SideScroller::drawTo(sf::RenderWindow& window)
 {
+	window.draw(playerShape);
 }
 
 void SideScroller::processKeyboardInput()
--- PA8/SideScroller.h
+++ PA8/SideScroller.h
@@ -2,15 +2,23 @@
 #define SIDE_SCROLLER_H
 
 #include "Screen.h"
+#include <SFML/Graphics.hpp>
 
 class SideScroller : public Screen
 {
 public:
+
+	SideScroller();
+
 	void drawTo(sf::RenderWindow& window);
 	void processKeyboardInput();
 	void processMousePosition(sf::Vector2i mouseWindowPosition);
 	void processMouseClick();
 	bool shouldExitGame();
+
+private:
+
+	sf::RectangleShape playerShape;
 };
 
 #endif // !SIDE_SCROLLER_H

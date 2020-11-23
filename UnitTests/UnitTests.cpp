91bb9dc0e7974a3bc2fda871ba4bfb774f9d8fbc
Beder Bourahmah <bederbourahmah@gmail.com>
2020-11-22 18:07:59 -0800

    Added unit test to check if overlapping rectangles indicate that they collided.

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -1,16 +1,24 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "MoveableRectangle.cpp"
+#include <SFML/Graphics.hpp>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTests
 {
-	TEST_CLASS(UnitTests)
+	TEST_CLASS(MoveableRectangleTests)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(OverlappingRectanglesDidCollideTrue)
 		{
+			MoveableRectangle leftRectangle = MoveableRectangle(sf::Vector2f(10.0f, 10.0f));
+			MoveableRectangle rightRectangle = MoveableRectangle(sf::Vector2f(10.0f, 10.0f));
+			leftRectangle.moveTo(100.0f, 100.0f);
+			rightRectangle.moveTo(100.0f, 109.99f);
+			Assert::IsTrue(leftRectangle.didCollideWithOtherComponent(rightRectangle));
+			Assert::IsTrue(rightRectangle.didCollideWithOtherComponent(leftRectangle));
 		}
 	};
 }

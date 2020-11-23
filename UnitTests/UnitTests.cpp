2d82aef4ce0eb95714f08d4c40bcbe6a6a7b9dda
Beder Bourahmah <bederbourahmah@gmail.com>
2020-11-22 19:13:15 -0800

    Added unit test for non overlapping rectangles reporting did collide as false.

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -20,5 +20,15 @@ namespace UnitTests
 			Assert::IsTrue(leftRectangle.didCollideWithOtherComponent(rightRectangle));
 			Assert::IsTrue(rightRectangle.didCollideWithOtherComponent(leftRectangle));
 		}
+
+		TEST_METHOD(NonOverlappingRectanglesDidCollideFalse)
+		{
+			MoveableRectangle leftRectangle = MoveableRectangle(sf::Vector2f(10.0f, 10.0f));
+			MoveableRectangle rightRectangle = MoveableRectangle(sf::Vector2f(10.0f, 10.0f));
+			leftRectangle.moveTo(100.0f, 100.0f);
+			rightRectangle.moveTo(100.0f, 110.01f);
+			Assert::IsFalse(leftRectangle.didCollideWithOtherComponent(rightRectangle));
+			Assert::IsFalse(rightRectangle.didCollideWithOtherComponent(leftRectangle));
+		}
 	};
 }

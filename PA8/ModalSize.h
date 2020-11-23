286d664a69423b3f896797aa3939630a98a73679
Beder Bourahmah <bederbourahmah@gmail.com>
2020-11-22 21:08:42 -0800

    Added docs for enums.

--- PA8/ModalSize.h
+++ PA8/ModalSize.h
@@ -1,6 +1,9 @@
 #ifndef MODAL_SIZE_H
 #define MODAL_SIZE_H
 
+/// <summary>
+/// Enum representing the predefined modal sizes.
+/// </summary>
 enum class ModalSize
 {
 	Small,
--- PA8/Screens.h
+++ PA8/Screens.h
@@ -1,6 +1,9 @@
 #ifndef SCREENS_H
 #define SCREENS_H
 
+/// <summary>
+/// Enum representing the various screens.
+/// </summary>
 enum class Screens
 {
 	MainMenu,
